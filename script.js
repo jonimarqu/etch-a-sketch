e1ad1bb5030c5c9ce013a5c652a58e1a3ab51ea1
jonimarqu <marquez.jonas91@gmail.com>
2022-11-14 18:59:58 -0300

    Add newGrid button.

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
-let gridSize = prompt(`Enter grid size:`, `4`);
+let gridSize = 20;
 
-const container = document.querySelector(`div`);
+const container = document.querySelector(`.container`);
 
 const grid = document.createElement(`div`);
 container.appendChild(grid);
 
 grid.style.display = `grid`;
-grid.style.width = `500px`;
-grid.style.height = `500px`;
-grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-
-console.log(container);
-
-for (let i = 1 ; i <= gridSize ** 2 ; i++) {
-
-    const square = document.createElement(`div`);
-    square.classList.add(`square`);
-    square.addEventListener(`mousedown`, changeBg);
-    square.addEventListener(`mousedown`, changeBg);
-    grid.appendChild(square);
-};
 
 const square = document.getElementsByClassName(`square`);
+
+function changeBg(e) {
+    if ( e.type == `mouseover`) {
+        e.target.style.backgroundColor = `#333333`;
+    };
+};
+
+const newGrid = document.getElementById("newGrid");
+newGrid.addEventListener("click", function() {
+    gridSize = prompt("Enter new grid size (120 max)", "64");
+    while ( 
+        gridSize <= 0 || 
+        gridSize > 120 || 
+        gridSize == !Number ) {
+    gridSize = prompt(`Enter a valid new grid size. Between 0 and 120.`, `64`);
+    if (gridSize == null ) {break};
+    };
+    buildGrid(gridSize);
+
+});
+
+function buildGrid() {
+    grid.style.width = `800px`;
+    grid.style.height = `800px`;
+    grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    grid.innerHTML = ``;
+    for (let i = 1 ; i <= gridSize ** 2 ; i++) {
+
+        const square = document.createElement(`div`);
+        square.classList.add(`square`);
+        square.addEventListener(`mouseover`, changeBg);
+    //    square.addEventListener(`mousedown`, changeBg);
+        grid.appendChild(square);
+    };
+};
+
+buildGrid(gridSize);
